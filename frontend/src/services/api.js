1c0d1110012a25a7dcf7adb3cfb04d6ec25db9bb
Jcffxr <eljefedelabiblia2005@gmail.com>
2025-12-12 00:45:07 -0400

    fix: Actualizar API URL al nuevo servidor v2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.PROD 
-        ? 'https://refugio-katze.onrender.com/api' // Si está en Vercel (Producción) usa esto
+        ? 'https://katze-api-v2.onrender.com/api' // Si está en Vercel (Producción) usa esto
         : 'http://localhost:4000/api',             // Si está en tu PC (Desarrollo) usa esto
 });
 
